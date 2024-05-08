5d6f029d8deb8bd38dc180bf93d6af6cae6ba019
Iván Camilo Fuertes Narváez <ivan.fuertes@icloud.com>
2024-05-08 11:17:46 -0500

    Tip total items are zero if the check total amount is zero

--- Project01_WeSplit/WeSplit/TipCalculatorTests/TipCalculatorTests.swift
+++ Project01_WeSplit/WeSplit/TipCalculatorTests/TipCalculatorTests.swift
@@ -3,8 +3,28 @@
 
 
 import XCTest
-@testable import TipCalculator
+
+class CalculateTip {
+    typealias TipTotal = (tipOverTotal: Double, totalPlusTip: Double, totalPerPerson: Double)
+    
+    func calculate(forCheckTotal: Double, withTipPercentage: UInt, dividedBetween: UInt) -> TipTotal {
+        return (0, 0, 0)
+    }
+}
 
 final class TipCalculatorTests: XCTestCase {
+    
+    func test_checkTotalIsZero_tipTotalIsZero() {
+        let checkTotal: Double = 0
+        let totalPeople: UInt = 1
+        let tipPercentage: UInt = 10
+        let sut = CalculateTip()
+        
+        let tipTotal = sut.calculate(forCheckTotal: checkTotal, withTipPercentage: tipPercentage, dividedBetween: totalPeople)
+        
+        XCTAssertEqual(tipTotal.tipOverTotal, 0)
+        XCTAssertEqual(tipTotal.totalPlusTip, 0)
+        XCTAssertEqual(tipTotal.totalPerPerson, 0)
+    }
     
 }
