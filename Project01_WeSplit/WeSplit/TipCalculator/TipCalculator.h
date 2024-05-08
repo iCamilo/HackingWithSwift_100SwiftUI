4b9d2921c52d1fe1ca8f8649c14bda0ecb96e87c
Iván Camilo Fuertes Narváez <ivan.fuertes@icloud.com>
2024-05-08 10:58:28 -0500

    Add TipCalculator Mac Framework

---
+++ Project01_WeSplit/WeSplit/TipCalculator/TipCalculator.h
@@ -0,0 +1,15 @@
+// WeSplit
+// Copyright ©️ 2024 Iván Camilo Fuertes. All rights reserved.
+
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TipCalculator.
+FOUNDATION_EXPORT double TipCalculatorVersionNumber;
+
+//! Project version string for TipCalculator.
+FOUNDATION_EXPORT const unsigned char TipCalculatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TipCalculator/PublicHeader.h>
+
+
---
+++ Project01_WeSplit/WeSplit/TipCalculatorTests/TipCalculatorTests.swift
@@ -0,0 +1,10 @@
+// WeSplit
+// Copyright ©️ 2024 Iván Camilo Fuertes. All rights reserved.
+
+
+import XCTest
+@testable import TipCalculator
+
+final class TipCalculatorTests: XCTestCase {
+    
+}
