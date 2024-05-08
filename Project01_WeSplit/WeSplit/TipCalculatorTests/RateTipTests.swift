f3b8d50566c71552f70de29c6e5c38e6d7e76f8a
Iván Camilo Fuertes Narváez <ivan.fuertes@icloud.com>
2024-05-08 14:15:32 -0500

    Tips > 5 and <= 20 should be rated as good

--- Project01_WeSplit/WeSplit/TipCalculatorTests/RateTipTests.swift
+++ Project01_WeSplit/WeSplit/TipCalculatorTests/RateTipTests.swift
@@ -6,11 +6,17 @@ import XCTest
 
 enum RateTipResult {
     case low
+    case good
 }
 
 struct RateTip {
     func rate(tip: UInt) -> RateTipResult {
-        .low
+        switch tip {
+        case 0...5:
+            return .low
+        default:
+            return .good
+        }
     }
 }
 
@@ -19,14 +25,16 @@ final class RateTipTests: XCTestCase {
         typealias TestCase = (tip: UInt, expected: RateTipResult, message: String)
         let tests: [TestCase] = [
             (tip: 5, expected: .low, message: "Tips <= 5 should be rated as low"),
-            (tip: 0, expected: .low, message: "Tips <= 5 should be rated as low")            
+            (tip: 0, expected: .low, message: "Tips <= 5 should be rated as low"),
+            (tip: 6, expected: .good, message: "Tips > 5 and <= 20 should be rated as good"),
+            (tip: 20, expected: .good, message: "Tips > 5 and <= 20 should be rated as good")           
         ]
         
         let sut = RateTip()
         
         for test in tests {
             let rate = sut.rate(tip: test.tip)
-            XCTAssertEqual(rate, test.expected, test.message)
+            XCTAssertEqual(rate, test.expected, "\(test.tip)% tip: \(test.message)" )
         }
     }
 }
