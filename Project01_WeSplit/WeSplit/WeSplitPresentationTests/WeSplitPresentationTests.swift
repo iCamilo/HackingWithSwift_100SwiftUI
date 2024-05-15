d34d4a326bb28e32b4b57aa770d21a99c36830f3
Iván Camilo Fuertes Narváez <ivan.fuertes@icloud.com>
2024-05-15 12:33:40 -0500

    Format tip results using NumberFormatter

--- Project01_WeSplit/WeSplit/WeSplitPresentationTests/WeSplitPresentationTests.swift
+++ Project01_WeSplit/WeSplit/WeSplitPresentationTests/WeSplitPresentationTests.swift
@@ -7,6 +7,18 @@ import WeSplitTipCalculator
 @testable import WeSplitPresentation
 
 struct WeSplitViewModel {
+    private lazy var tipFormatter: NumberFormatter = {
+        let resultFormatter = NumberFormatter()
+        resultFormatter.numberStyle = .currency
+        resultFormatter.minimumFractionDigits = 0
+        resultFormatter.maximumFractionDigits = 2
+        resultFormatter.currencyDecimalSeparator = "."
+        resultFormatter.currencySymbol = "$"
+        resultFormatter.alwaysShowsDecimalSeparator = false
+        
+        return resultFormatter
+    }()
+    
     private(set) var tipCalculator: TipCalculator
     
     init(tipCalculator: TipCalculator) {
@@ -40,14 +52,17 @@ struct WeSplitViewModel {
         guard let checkTotal = Double(checkTotal) else {
             return
         }
+        guard let tipTotal = try? tipCalculator.calculate(forCheckTotal: checkTotal, withTipPercentage: tip, dividedBetween: totalPeople) else { return }
         
-        guard let result = try? tipCalculator.calculate(forCheckTotal: checkTotal, withTipPercentage: tip, dividedBetween: totalPeople) else {
-            return
-        }
-        
-        tipOverTotal = "Tip Over Total: $\(result.tipOverTotal)"
-        tipPlusTip = "Tip plus Tip: $\(result.totalPlusTip)"
-        totalPerPerson = "Total per Person: $\(result.totalPerPerson)"
+        tipOverTotal = "Tip Over Total: \(tipFormatter.string(from: tipTotal.tipOverTotal))"
+        tipPlusTip = "Tip plus Tip: \(tipFormatter.string(from: tipTotal.totalPlusTip))"
+        totalPerPerson = "Total per Person: \(tipFormatter.string(from: tipTotal.totalPerPerson))"
+    }
+}
+
+private extension NumberFormatter {
+    func string(from double: Double) -> String {
+        self.string(from: double as NSNumber) ?? ""
     }
 }
 
@@ -83,14 +98,14 @@ final class WeSplitPresentationTests: XCTestCase {
     }
     
     func test_calculateTip_updateTipResults() {
-        let tipTotal: WeSplitTipCalculator.TipTotal = .init(tipOverTotal: 1, totalPlusTip: 100.1, totalPerPerson: 50.05)
+        let tipTotal: WeSplitTipCalculator.TipTotal = .init(tipOverTotal: 1, totalPlusTip: 100.099, totalPerPerson: 50.9278)
         var (sut, _) = makeSUT(tipTotal: tipTotal)
         
         sut.checkTotal = "100"
-        
-        XCTAssertEqual(sut.tipOverTotal, "Tip Over Total: $1.0")
-        XCTAssertEqual(sut.tipPlusTip, "Tip plus Tip: $100.1")
-        XCTAssertEqual(sut.totalPerPerson, "Total per Person: $50.05")
+       
+        XCTAssertEqual(sut.tipOverTotal, "Tip Over Total: $ 1")
+        XCTAssertEqual(sut.tipPlusTip, "Tip plus Tip: $ 100.1")
+        XCTAssertEqual(sut.totalPerPerson, "Total per Person: $ 50.93")
     }
     
     // MARK: - Utils
