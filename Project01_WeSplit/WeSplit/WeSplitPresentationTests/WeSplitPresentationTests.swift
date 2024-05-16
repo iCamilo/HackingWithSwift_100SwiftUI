f66250f636223339a6a878724cd692c3eb4c9744
Iván Camilo Fuertes Narváez <ivan.fuertes@icloud.com>
2024-05-15 20:44:17 -0500

    Make init fail-able when maxPartySize is less than one or tip options is empty

--- Project01_WeSplit/WeSplit/WeSplitPresentationTests/WeSplitPresentationTests.swift
+++ Project01_WeSplit/WeSplit/WeSplitPresentationTests/WeSplitPresentationTests.swift
@@ -54,12 +54,24 @@ struct TipTotalResult {
 struct WeSplitViewModel {
     private let tipCalculator: TipCalculator
     private(set) var tipOptions: [TipOption]
+    private(set) var totalPeopleRange: ClosedRange<Int>
     
-    init(tipCalculator: TipCalculator, tipOptions: [TipOption]) {
+    ///  ViewModel used to capture the required data to calculate a check tip. Tip calculation results are stored in `tipTotalResult`
+    ///
+    /// - Precondition: `maxPartySize` should be greater than one
+    /// - Precondition:  `tipOptions` should not be empty
+    init?(tipCalculator: TipCalculator, tipOptions: [TipOption], maxPartySize: UInt) {
+        guard maxPartySize >= 1,
+            !tipOptions.isEmpty
+        else {
+            return nil
+        }
+        
         self.tipCalculator = tipCalculator
         self.tipOptions = tipOptions
         self.totalPeople = .init(value: 1)
         self.tip = tipOptions.first ?? .init(value: 0)
+        self.totalPeopleRange = 1...Int(maxPartySize)
     }
     
     var checkTotal: String = "" {
@@ -109,6 +121,22 @@ private extension NumberFormatter {
 
 final class WeSplitPresentationTests: XCTestCase {
     
+    func test_initWithPartySizeZero_shouldFail() {
+        var (sut, _) = makeSUT(maxPartySize: 0)
+        XCTAssertNil(sut, "Init should fail if party size is less than one")
+        
+        (sut, _) = makeSUT(maxPartySize: 1)
+        XCTAssertNotNil(sut, "Init should NOT fail if party size is at least one")
+    }
+    
+    func test_initWithEmptyTipsOptions_shouldFail() {
+        var (sut, _) = makeSUT(tipOptions: [])
+        XCTAssertNil(sut, "Init should fail if party size is less than one")
+        
+        (sut, _) = makeSUT(tipOptions: [.init(value: 0)])
+        XCTAssertNotNil(sut, "Init should NOT fail if party size is at least one")
+    }
+    
     func test_tipOptions_descriptionsShowTipAsPercentages() {
         let tipOptions: [TipOption] = [
             .init(value: 0),
@@ -117,30 +145,30 @@ final class WeSplitPresentationTests: XCTestCase {
         
         let (sut, _) = makeSUT(tipOptions: tipOptions)
         
-        XCTAssertEqual(sut.tipOptions.map { $0.description },
+        XCTAssertEqual(sut?.tipOptions.map { $0.description },
                        ["0%", "5%"])
     }
     
     func test_totalPeopleDescription_showPluralWhenMoreThanOne() {
         var (sut, _) = makeSUT()
         
-        sut.totalPeople.value = 1
-        XCTAssertEqual(sut.totalPeople.description, "1 person")
+        sut?.totalPeople.value = 1
+        XCTAssertEqual(sut?.totalPeople.description, "1 person")
         
-        sut.totalPeople.value = 2
-        XCTAssertEqual(sut.totalPeople.description, "2 people")
+        sut?.totalPeople.value = 2
+        XCTAssertEqual(sut?.totalPeople.description, "2 people")
     }
     
     func test_showTotalIfCheckTotalIsANumber() {
         var (sut, _) = makeSUT()
         
-        sut.checkTotal = ""
+        sut?.checkTotal = ""
         assert(sut, showTotal: false)
         
-        sut.checkTotal = "invalid"
+        sut?.checkTotal = "invalid"
         assert(sut, showTotal: false)
         
-        sut.checkTotal = "100"
+        sut?.checkTotal = "100"
         assert(sut, showTotal: true)
     }
     
@@ -150,25 +178,26 @@ final class WeSplitPresentationTests: XCTestCase {
         calculator.makeCalculateSucceed(with: TipTotal(tipOverTotal: 10,
                                                        totalPlusTip: 100.10,
                                                        totalPerPerson: 100.10))
-        sut.checkTotal = "100"
+        sut?.checkTotal = "100"
         assert(sut, showTotal: true)
         
         calculator.makeCalculateFail()
-        sut.checkTotal = "100"
+        sut?.checkTotal = "100"
         assert(sut, showTotal: false)
     }
     
     func test_changeAnyInputParameter_shouldReCalculateAndFormatTipResults() {
+        let tipOptions = [TipOption(value: 0), TipOption(value: 10)]
         let tipTotal: TipTotal = .init(tipOverTotal: 10,
                                        totalPlusTip: 100.10,
                                        totalPerPerson: 33.366666)
-        var (sut, calculator) = makeSUT()
+        var (sut, calculator) = makeSUT(tipOptions: tipOptions)
         calculator.makeCalculateSucceed(with: tipTotal)
         assert(sut, showTotal: false)
         
-        sut.checkTotal = "100"
-        sut.tip.value = 10
-        sut.totalPeople.value = 3
+        sut?.checkTotal = "100"
+        sut?.tip.value = 10
+        sut?.totalPeople.value = 3
         
         XCTAssertEqual(
             calculator.messages,
@@ -179,16 +208,18 @@ final class WeSplitPresentationTests: XCTestCase {
         )
         
         assert(sut, showTotal: true)
-        XCTAssertEqual(sut.tipTotalResult?.tipOverTotal, "Tip Over Total: $10.00")
-        XCTAssertEqual(sut.tipTotalResult?.tipPlusTip, "Tip plus Tip: $100.10")
-        XCTAssertEqual(sut.tipTotalResult?.totalPerPerson, "Total per Person: $33.37")
+        XCTAssertEqual(sut?.tipTotalResult?.tipOverTotal, "Tip Over Total: $10.00")
+        XCTAssertEqual(sut?.tipTotalResult?.tipPlusTip, "Tip plus Tip: $100.10")
+        XCTAssertEqual(sut?.tipTotalResult?.totalPerPerson, "Total per Person: $33.37")
     }
     
     
     // MARK: - Utils
-    func makeSUT(tipOptions: [TipOption] = []) -> (sut: WeSplitViewModel, tipCalculator: TipCalculatorSpy) {
+    func makeSUT(tipOptions: [TipOption] = [.init(value: 100)], maxPartySize: UInt = 1) -> (sut: WeSplitViewModel?, tipCalculator: TipCalculatorSpy) {
         let tipCalculator = TipCalculatorSpy()
-        let sut = WeSplitViewModel(tipCalculator: tipCalculator, tipOptions: tipOptions)
+        let sut = WeSplitViewModel(tipCalculator: tipCalculator,
+                                   tipOptions: tipOptions,
+                                   maxPartySize: maxPartySize)
         
         tipCalculator.makeCalculateSucceed(with: TipTotal(tipOverTotal: 0,
                                                           totalPlusTip: 0,
@@ -197,23 +228,23 @@ final class WeSplitPresentationTests: XCTestCase {
         return (sut, tipCalculator)
     }
     
-    private func assert(_ sut: WeSplitViewModel, showTotal: Bool, file: StaticString = #filePath, line: UInt = #line) {
-        XCTAssertEqual(sut.showTotal, showTotal,
+    private func assert(_ sut: WeSplitViewModel?, showTotal: Bool, file: StaticString = #filePath, line: UInt = #line) {
+        XCTAssertEqual(sut?.showTotal, showTotal,
                        "Should \(showTotal ? "" : "NOT") show Total",
                        file: file, line: line)
         
         if !showTotal {
-            XCTAssertNil(sut.tipTotalResult,
+            XCTAssertNil(sut?.tipTotalResult,
                          "Tip total results should be nil when total is not showing",
                          file: file, line: line)
         } else {
-            XCTAssertEqual(sut.tipTotalResult?.tipOverTotal.isEmpty, false,
+            XCTAssertEqual(sut?.tipTotalResult?.tipOverTotal.isEmpty, false,
                            "Tip over Total should NOT be empty",
                            file: file, line: line)
-            XCTAssertEqual(sut.tipTotalResult?.tipPlusTip.isEmpty, false,
+            XCTAssertEqual(sut?.tipTotalResult?.tipPlusTip.isEmpty, false,
                            "Tip plus Tip should NOT be empty",
                            file: file, line: line)
-            XCTAssertEqual(sut.tipTotalResult?.totalPerPerson.isEmpty, false,
+            XCTAssertEqual(sut?.tipTotalResult?.totalPerPerson.isEmpty, false,
                            "Tip total per person should NOT be empty",
                            file: file, line: line)
         }
