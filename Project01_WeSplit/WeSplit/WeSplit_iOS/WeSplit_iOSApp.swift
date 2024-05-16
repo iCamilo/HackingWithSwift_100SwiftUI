118df08a93c91eca07c3cc1adaa8fe43141f72e8
Iván Camilo Fuertes Narváez <ivan.fuertes@icloud.com>
2024-05-16 17:23:29 -0500

    Add WeSplit_iOS App

---
+++ Project01_WeSplit/WeSplit/WeSplit_iOS/WeSplit_iOSApp.swift
@@ -0,0 +1,14 @@
+// WeSplit
+// Copyright ©️ 2024 Iván Camilo Fuertes. All rights reserved.
+
+
+import SwiftUI
+
+@main
+struct WeSplit_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            
+        }
+    }
+}
