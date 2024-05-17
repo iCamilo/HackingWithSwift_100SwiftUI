7506eeb0daa5f15fe7127ac532966da6a7dce821
Iván Camilo Fuertes Narváez <ivan.fuertes@icloud.com>
2024-05-17 18:32:57 -0500

    Use a factory method to init WeSplitView

--- Project01_WeSplit/WeSplit/WeSplit-watchOS Watch App/WeSplit_watchOSApp.swift
+++ Project01_WeSplit/WeSplit/WeSplit-watchOS Watch App/WeSplit_watchOSApp.swift
@@ -10,10 +10,26 @@ import WeSplitPresentation
 struct WeSplit_watchOS_Watch_AppApp: App {
     var body: some Scene {
         WindowGroup {
-            let tipOptions = [TipOption(value: 0), TipOption(value: 10), TipOption(value: 50)]
-            let viewModel = WeSplitViewModel(tipCalculator: CalculateTip(), tipRater: RateTip(), tipOptions: tipOptions, maxPartySize: 5)!
-            
-            WeSplitView(viewModel: viewModel)
+           makeWeSplitView()
         }
+    }
+    
+    private func makeWeSplitView() -> WeSplitView {
+        let tipOptions = [TipOption(value: 0), 
+                          TipOption(value: 10),
+                          TipOption(value: 20),
+                          TipOption(value: 25),
+                          TipOption(value: 50)]
+        guard let viewModel = WeSplitViewModel(
+            tipCalculator: CalculateTip(),
+            tipRater: RateTip(),
+            tipOptions: tipOptions,
+            maxPartySize: 10
+        )
+        else {
+            fatalError("Failed to init WeSplitViewModel")
+        }
+        
+        return WeSplitView(viewModel: viewModel)
     }
 }
