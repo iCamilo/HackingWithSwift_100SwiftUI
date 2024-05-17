bb69578264c41c61979000ad7a784f17720c0eec
Iván Camilo Fuertes Narváez <ivan.fuertes@icloud.com>
2024-05-17 14:04:02 -0500

    Use util methods to create each step view

--- Project01_WeSplit/WeSplit/WeSplit-watchOS Watch App/WeSplitView.swift
+++ Project01_WeSplit/WeSplit/WeSplit-watchOS Watch App/WeSplitView.swift
@@ -10,26 +10,62 @@ struct WeSplitView: View {
     
     var body: some View {
         TabView {
-            TextField("Check", text: $viewModel.checkTotal)
-            
-            Picker("Tip",selection: $viewModel.tip.value) {
-                ForEach(viewModel.tipOptions, id: \.self.value) {
-                    Text($0.description)
-                }
+            stepView("1/3 Enter Check Total") {
+                TextField("", text: $viewModel.checkTotal)
             }
             
-            Stepper(value: $viewModel.totalPeople.value, in: viewModel.totalPeopleRange) {
-                Text(viewModel.totalPeople.description)
-                    .font(.callout)
+            stepView("2/3 Select Tip") {
+                tipPicker
             }
             
-            List {
+            stepView("3/3 Divide between") {
+                totalPeopleSelector
+            }
+                        
+            stepView("Total", titleColor: .red) {
+                totalView
+            }
+        }
+        .tabViewStyle(.automatic)
+    }
+    
+    private func stepView(_ title: String, titleColor: Color = .blue, content: () -> some View) -> some View {
+        VStack(alignment: .leading, spacing: 15) {
+            Text(title)
+                .font(.caption)
+                .foregroundStyle(titleColor)
+            
+            content()
+            
+            Spacer()
+        }
+        .padding(.horizontal)
+    }
+    
+    private var tipPicker: some View {
+        Picker("",selection: $viewModel.tip.value) {
+            ForEach(viewModel.tipOptions, id: \.self.value) {
+                Text($0.description)
+            }
+        }
+    }
+    
+    private var totalPeopleSelector: some View {
+        Stepper(value: $viewModel.totalPeople.value, in: viewModel.totalPeopleRange) {
+            Text(viewModel.totalPeople.description)
+                .font(.callout)
+        }
+    }
+    
+    private var totalView: some View {
+        List() {
+            if viewModel.showTotal{
                 Text(viewModel.tipTotalResult?.tipOverTotal ?? "")
                 Text(viewModel.tipTotalResult?.tipPlusTip ?? "")
                 Text(viewModel.tipTotalResult?.totalPerPerson ?? "")
+            } else {
+                Text("Enter Check Total to get results")
             }
-            
         }
-        .tabViewStyle(.automatic)
     }
 }
